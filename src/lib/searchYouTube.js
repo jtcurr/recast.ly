bafc2042a9914fcb29464284cc87cbc731b04883
jtcurr <johntcurrie@gmail.com>
2017-01-07 16:00:25 -0800

    Implement searchYoutube and pass specs

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,27 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    dataType: 'json',
+    data: {
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      part: 'snippet',
+      order: 'rating',
+      type: 'video',
+      videoDefinition: 'high',
+      videoEmbeddable: true
+    },
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('Videos Recieved!', data);
+      callback(data.items);
+    },
+    error: function(data) {
+      console.log('ERROR', data);
+    } 
+  });
 };
 
 window.searchYouTube = searchYouTube;
